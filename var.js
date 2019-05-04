ee894c8d47c8117c7ef8119bb99955991e6d6d74
Kiet <bangminhkiet@gmail.com>
2019-05-04 22:20:10 +0700

    Hoàn tất single-product.html thiếu mỗi mô tả sản phẩm nếu muốn thì làm luôn cũng được

---
+++ var.js
@@ -0,0 +1,275 @@
+﻿const data =
+    [
+        {
+            Id: "1",
+            LinkImageSP: "img/AbsolutRaspberri_thumb.jpg",
+            NameSP: "Vodka Absolut Raspberri",
+            SaleSP: 300000,
+            money: 310000,
+            Category: "Rượu Thụy Điển",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Thụy Điển"]
+        },
+
+        {
+            Id: "2",
+            LinkImageSP: "img/antoine grenache cabernet sauvignon.jpg",
+            NameSP: "Antoine Grenache Cabernet Sauvignon",
+            SaleSP: 160000,
+            money: 170000,
+            Category: "Rượu Pháp",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "3",
+            LinkImageSP: "img/antoine grenache merlot.jpg",
+            NameSP: "Antoine Grenache Merlot",
+            SaleSP: 160000,
+            money: 170000,
+            Category: "Rượu Pháp",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "4",
+            LinkImageSP: "img/antoine grenache shiraz.jpg",
+            NameSP: "Antoine Grenache Shiraz",
+            SaleSP: 160000,
+            money: 170000,
+            Category: "Rượu Pháp",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "5",
+            LinkImageSP: "img/chivas-12-nam-ruoungoaigiasi.vn-500x500.jpg",
+            NameSP: "Chivas Regal 12",
+            SaleSP: 550000,
+            money: 600000,
+            Category: "Rượu Scotland",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Scotland"]
+        },
+
+        {
+            Id: "6",
+            LinkImageSP: "img/COGNAC CAMUS.jpg",
+            NameSP: "Camus XO Elegance",
+            SaleSP: 2700000,
+            money: 2900000,
+            Category: "Rượu Pháp",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "7",
+            LinkImageSP: "img/CognacXOBahia_thumb.jpg",
+            NameSP: "Cognac X.O Bahia",
+            SaleSP: 500000,
+            money: 520000,
+            Category: "Rượu Pháp ",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "8",
+            LinkImageSP: "img/CONDO CABERNET SAUVIGNON SWAN REACH 2013.jpg",
+            NameSP: "Condo Cabernet Sauvignon Swan Reach 2013",
+            SaleSP: 750000,
+            money: 770000,
+            Category: "Rượu Úc",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Úc"]
+        },
+
+        {
+            Id: "9",
+            LinkImageSP: "img/CONDO CHARDONNAY SWAN REACH 2015.jpg",
+            NameSP: "Condo Chardonnay Swan Reach 2015",
+            SaleSP: 750000,
+            money: 770000,
+            Category: "Rượu Úc",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Úc"]
+        },
+
+        {
+            Id: "10",
+            LinkImageSP: "img/CONDO SHIRAZ SWAN REACH 2013.jpg",
+            NameSP: "Condo Shiraz Swan Reach 2013",
+            SaleSP: 750000,
+            money: 770000,
+            Category: "Rượu Úc",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Úc"]
+        },
+
+        {
+            Id: "11",
+            LinkImageSP: "img/CORPALO GRAN RESERVA, SHIRAZ-MERLOT.jpg",
+            NameSP: "Corpalo Gran reserva, Shiraz-Merlot",
+            SaleSP: 650000,
+            money: 680000,
+            Category: "Rượu Chile",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Chile"]
+        },
+
+        {
+            Id: "12",
+            LinkImageSP: "img/Deinhard Riesling.jpg",
+            NameSP: "Deinhard Riesling",
+            SaleSP: 400000,
+            money: 410000,
+            Category: "Rượu Đức",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Đức"]
+        },
+
+        {
+            Id: "13",
+            LinkImageSP: "img/louis iii xo.jpg",
+            NameSP: "Louis III XO",
+            SaleSP: 580000,
+            money: 600000,
+            Category: "Rượu Pháp",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "14",
+            LinkImageSP: "img/Maison Francaise.png",
+            NameSP: "Maison Francaise",
+            SaleSP: 150000,
+            money: 160000,
+            Category: "Rượu Pháp",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "15",
+            LinkImageSP: "img/MAPOLA GRAN RESERVA , CARBERNET SAUVIGNON-MERLOT.jpg",
+            NameSP: "Mapola Gran reserva , Carbernet Sauvignon-merlot",
+            SaleSP: 700000,
+            money: 730000,
+            Category: "Rượu Chile",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Chile"]
+        },
+
+        {
+            Id: "16",
+            LinkImageSP: "img/nuoc Ep patriti nho Denjpg.jpg",
+            NameSP: "PATRITI Nho Đen",
+            SaleSP: 100000,
+            money: 105000,
+            Category: "Rượu Australian",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Australian"]
+        },
+
+        {
+            Id: "17",
+            LinkImageSP: "img/PAVO REAL Cabernet Sauvignon-Camenere.png",
+            NameSP: "PAVO REAL Cabernet ",
+            SaleSP: 650000,
+            money: 680000,
+            Category: "Rượu Chile",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Chile"]
+        },
+
+        {
+            Id: "18",
+            LinkImageSP: "img/RAP SPECIAL RESERVA , CAMERNERE.jpg",
+            NameSP: "Rap Special Reserva , Camernere",
+            SaleSP: 350000,
+            money: 390000,
+            Category: "Rượu Chile",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Chile"]
+        },
+
+        {
+            Id: "19",
+            LinkImageSP: "img/ruou-chivas-18-nam-hop-xanh.jpg",
+            NameSP: "Chivas Regal 18 Year",
+            SaleSP: 1100000,
+            money:  1150000,
+            Category: "Rượu Scotland",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Scotland"]
+        },
+
+        {
+            Id: "20",
+            LinkImageSP: "img/ruou-vang-argalis-750ml.jpg",
+            NameSP: "Rượu Vang Pháp - ARGALIS",
+            SaleSP: 140000,
+            money: 155000,
+            Category: "Rượu Pháp",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "21",
+            LinkImageSP: "img/sieu_thi_ruou_ngoai_baron_otard_thumb.jpg",
+            NameSP: "Baron Otard VSOP Cognac",
+            SaleSP: 1050000,
+            money: 1100000,
+            Category: "Rượu Pháp",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Pháp"]
+        },
+
+        {
+            Id: "22",
+            LinkImageSP: "img/Sparkling Nga do.png",
+            NameSP: "Sparkling Nga đỏ",
+            SaleSP: 100000,
+            money: 120000,
+            Category: "Rượu Nga",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Nga"]
+        },
+
+        {
+            Id: "23",
+            LinkImageSP: "img/STAGRE LIMITED EDITION , CARBERNET SAUVIGNON-SHIRAZ.jpg",
+            NameSP: "Carbernet Sauvignon-Shiraz",
+            SaleSP: 700000,
+            money: 780000,
+            Category: "Rượu Chile",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Chile"]
+        },
+
+        {
+            Id: "24",
+            LinkImageSP: "img/Viento del Sursyrah cabernet sauvignon.jpg",
+            NameSP: "Viento del Sursyrah cabernet sauvignon",
+            SaleSP: 850000,
+            money: 1000000,
+            Category: "Rượu Chile",
+            LuotMua: 0,
+            Tag: ["Rượu", "Rượu Chile"]
+        }
+    ];
+
+function getItemById(id) {
+    console.log('ID: ' + typeof (id) + ' - ' + id);
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].Id == id) 
+			return data[i];
+    }
+    // return data[id];;
+}
